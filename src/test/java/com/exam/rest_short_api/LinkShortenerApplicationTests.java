99b88cd62fa5df1c49031e5f8e6d783fbba901ad
Q7T <homak477@gmail.com>
2023-09-24 19:16:48 +0500

    Initial comm

---
+++ src/test/java/com/exam/rest_short_api/LinkShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.rest_short_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
